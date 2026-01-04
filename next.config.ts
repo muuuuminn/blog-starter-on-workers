da5c809f12c4b852db340d83660ef20641bb370a
muuuuminn <hiro.041310402.kt@gmail.com>
2026-01-04 12:17:28 +0900

    first commit

---
+++ next.config.ts
@@ -0,0 +1,10 @@
+import type { NextConfig } from 'next'
+
+const nextConfig: NextConfig = {
+  reactStrictMode: true,
+}
+
+export default nextConfig
+
+import { initOpenNextCloudflareForDev } from '@opennextjs/cloudflare'
+initOpenNextCloudflareForDev()
